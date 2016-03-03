a8aa76c4ec2a67bd9c1d1a3e81b2fd9026caa024
Jay Mo <jayhxmo@gmail.com>
2016-03-03 17:37:02 -0500

    Add filter function

--- js/render.js
+++ js/render.js
@@ -1,4 +1,4 @@
-var UIs = [
+var all = [
 	{ day: 100, title: 'Hundred', tags: 'hundred, ui, congrats' },
 	{ day: 99, title: 'YG Artists', tags: 'singer, artist, k-pop' },
 	{ day: 98, title: 'Splash Screen', tags: 'loading, launch screen' },
@@ -101,11 +101,49 @@ var UIs = [
 	{ day: 1, title: 'Messaging App', tags: 'app, messaging' },
 ];
 
+var highlightIndex = [100, 99, 98, 94, 93, 92, 88, 86, 84, 82, 79, 78, 69, 63, 56, 55, 52, 51, 47, 46, 45, 44, 41, 40, 35, 32, 31, 30, 22, 21, 15, 14, 13, 11, 5, 2];
+var popularIndex = [100, 93, 90, 89, 88, 86, 84, 83, 82, 81, 79, 78, 73, 70, 63, 55, 52, 51, 48, 44, 41, 28, 15, 5];
+var highlights = [];
+var popular = [];
+
+var hIC = 0;
+var pIC = 0;
+all.forEach(function(entry) {
+	if (entry['day'] == highlightIndex[hIC]) {
+		highlights.push(entry);
+		hIC++;
+	}
+
+	if (entry['day'] == popularIndex[pIC]) {
+		popular.push(entry);
+		pIC++;
+	}
+});
+
+console.log(highlights);
+
+var UIs = highlights;
+
 var RepeatModule = React.createClass({
 	openModal: function(day) {
 		var s = $('html').scrollTop();    
 		$('html').css('overflow', 'hidden');
 		$('html').scrollTop(s);	
+
+		var UIindex = UIs.length - day;
+
+		if (UIs.length == highlights.length) {
+			console.log('Highlights');
+			UIindex = highlightIndex.indexOf(day);
+		}
+
+		else if (UIs.length == popular.length) {
+			console.log('Popular');
+			UIindex = popularIndex.indexOf(day);
+		}
+
+		console.log(UIindex);
+		console.log(UIs[UIindex]);
 
 		$.featherlight(
 			'<html>' +
@@ -117,8 +155,8 @@ var RepeatModule = React.createClass({
 					'</div>' +
 					'<div class="info">' +
 						'<h4 class="credits">Designed by <a href="http://dribbble.com/jayhxmo">Jay Mo</a></h4>' +
-						'<h2 class="title">Day ' + day + ' - ' + UIs[100 - day]['title'] + '</h2>' +
-						'<h4 class="tags">' + UIs[100 - day]['tags'] + '</h4>' +
+						'<h2 class="title">Day ' + day + ' - ' + UIs[UIindex]['title'] + '</h2>' +
+						'<h4 class="tags">' + UIs[UIindex]['tags'] + '</h4>' +
 					'</div>' +
 					'<div class="download">' +
 						'<form>' +
@@ -180,5 +218,26 @@ var RepeatModule = React.createClass({
 
 ReactDOM.render(<RepeatModule UIs={UIs} />, document.querySelector('main'));
 
-// data-mfp-src='shot.html?day=51'
-// {'images/UIs/Day ' + ui.day + ' - UI.jpg'}
+$("#all").click(function(){
+	UIs = all;
+	$('#all').addClass('active');
+	$('#highlights').removeClass('active');
+	$('#popular').removeClass('active');
+	ReactDOM.render(<RepeatModule UIs={UIs} />, document.querySelector('main'));
+});
+
+$('#highlights').click(function(){
+	UIs = highlights;
+	$('#all').removeClass('active');
+	$('#highlights').addClass('active');
+	$('#popular').removeClass('active');
+	ReactDOM.render(<RepeatModule UIs={UIs} />, document.querySelector('main'));
+});
+
+$('#popular').click(function(){
+	UIs = popular;
+	$('#all').removeClass('active');
+	$('#highlights').removeClass('active');
+	$('#popular').addClass('active');
+	ReactDOM.render(<RepeatModule UIs={UIs} />, document.querySelector('main'));
+});
