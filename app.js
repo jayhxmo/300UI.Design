1066b299a3e5e4ff348ad69ff0c95985cb3bd87b
Jay Mo <jayhxmo@gmail.com>
2016-03-16 15:34:48 -0400

    Add email template

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+var downloadEmail = [    
+    '<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial scale=1.0\"/><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css\"><style>@media only screen and (max-width: 1000px){h1{font-size: 28px !important;}h3{font-size: 18px !important;}}</style></head><body><div style=\"text-align: center; font-family: \'Helvetica\', \'Arial\', sans-serif;\"><center><h1 style=\"font-size: 54px; font-weight: bold;\">300 UI in 300 Days</h1><h3 style=\"font-size: 24px;\">Day ', 
+    // [day count] - [ui title]
+    '</h3><img src=\"',
+    // ui source
+    '\" style=\"display: block; width: 540px; max-width: 100%; margin: 10px auto;\"><a href=\"',
+    // download link
+    '\" style=\"display: block; width: 200px; height: 50px; border: 0; border-radius: 50px; background: #229aff; color: #fff; font-family: \'Helvetica\', \'Arial\', sans-serif; font-weight: bold; margin: 15px auto 7px; box-shadow: 0 5px 30px #229aff; line-height: 50px; text-decoration: none;\">DOWNLOAD</a><h6 style=\"font-size: 10px; margin: 15px auto 0 auto;\">It\'s also <a href=\"https://github.com/jayhxmo/300UI-in-300Days\">open source</a></h6><ul style=\"list-style: none; margin: 50px auto 0; display: inline-block; padding: 0;\"> <li style=\"display: inline-block; margin: 0 13px;\"><a href=\"https://twitter.com/jayhxmo\" target=\"_blank\" style=\"color: #555;\"><img src=\"https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/24/twitter.png\" alt=\"\"></a></li><li style=\"display: inline-block; margin: 0 13px;\"><a href=\"https://dribbble.com/jayhxmo\" target=\"_blank\" style=\"color: #555;\"><img src=\"https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/24/dribbble.png\" alt=\"\"></a></li><li style=\"display: inline-block; margin: 0 13px;\"><a href=\"https://www.behance.net/jayhxmo\" target=\"_blank\" style=\"color: #555;\"><img src=\"https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/24/behance.png\" alt=\"\"></a></li><li style=\"display: inline-block; margin: 0 13px;\"><a href=\"https://www.github.com/jayhxmo\" target=\"_blank\" style=\"color: #555;\"><img src=\"https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/24/github.png\" alt=\"\"></a></li><li style=\"display: inline-block; margin: 0 13px;\"><a href=\"https://www.linkedin.com/in/jayhxmo\" target=\"_blank\" style=\"color: #555;\"><img src=\"https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/png/24/linkedin.png\" alt=\"\"></a></li></center></ul></div></body></html>'
+];
+
 var session = require('express-session')
 var mongoose = require('mongoose');
 var nodemailer = require('nodemailer');
@@ -84,27 +94,29 @@ app.post('/download', function(req, res, next) {
             });
             newUser.save();
 
-            day.findOneAndUpdate({ day: 100 }, { $inc: { downloaded: 1 } }, function (err, doc) {});
+            // day.findOneAndUpdate({ day: 100 }, { $inc: { downloaded: 1 } }, function (err, doc) {});
 
             done(null, token, vip);
         },
         function(token, user, done) {
-            // transport.sendMail({
-            //     from: '300 UI in 300 Days <download@300ui.design>',
-            //     to: req.body.email,
-            //     subject: 'Download: Day ' + req.body.day + " - Calculator",
-            //     html: '<a href="300ui.design/download/' + token + '">Download</a>'
-            // },
+            transport.sendMail({
+                from: '300 UI in 300 Days <download@300ui.design>',
+                to: req.body.email,
+                subject: 'Download: Day ' + req.body.day + " - Calculator",
+                html: downloadEmail[0] + req.body.day + ' - ' + uiTitle +
+                      downloadEmail[1] + 'raw.githubusercontent.com/jayhxmo/300UI.Design/master/images/UIs/Day%20' + req.body.day +'%20-%20UI.jpg' +
+                      downloadEmail[2] + 'http://300ui.design/download/' + token + 
+                      downloadEmail[3]
+            },
+            function(err, info) {
+                if (err) {
+                    // console.error(err);
+                } 
 
-            // function(err, info) {
-            //     if (err) {
-            //         // console.error(err);
-            //     } 
-
-            //     else {
-            //         // console.log(info);
-            //     }
-            // });
+                else {
+                    // console.log(info);
+                }
+            });
 
             // Update and notify the user that the email has been sent
         }
@@ -115,6 +127,7 @@ app.post('/download', function(req, res, next) {
 });
 
 app.get('/download/:token', function(req, res) {
+    // res.send('sup');
     vip.findOne({ linkToken: req.params.token }, function(err, token) {
         if (!token) {
             console.log("Not found - now query all results");
